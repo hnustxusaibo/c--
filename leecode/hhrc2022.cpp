3bb347cfa78ed723ef1facafd4c35f25c8cbbb85
saibo-xu <saibo.xu@yitu-inc.com>
2022-10-08 19:30:53 +0800

    sss

---
+++ leecode/hhrc2022.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef pair<int,int> pii;
+const int mod = 1e9+7;
+const int MAX = 2e9+7;
+const int N = 2e4+5;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int minSupplyStationNumber(TreeNode* root) {
+
+    }
+};
