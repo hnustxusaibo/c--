be41e00b790aa6883b20a2ea203ec2e9f92ef617
saibo-xu <saibo.xu@yitu-inc.com>
2022-11-10 16:51:38 +0800

    aaa

--- leecode/864.cpp
+++ leecode/864.cpp
@@ -60,9 +60,9 @@ int shortestPathAllKeys(vector<string> &grid)
                 continue;
             if (mp[xx][yy] > 10 && (status & (1 << (mp[xx][yy] - 11))) == 0)
                 continue;
-            printf("%d\b",(status & (1 << (mp[xx][yy] - 11)));
+            printf("%d\b", (status & (1 << (mp[xx][yy] - 11))));
             dp[xx][yy][status] = 1;
-            if (mp[xx][yy] > 0 && mp[xx][yy] < 7 && (status&(1<<mp[xx][yy]-1))==0)
+            if (mp[xx][yy] > 0 && mp[xx][yy] < 7 && (status & (1 << mp[xx][yy] - 1)) == 0)
             {
                 printf("xx=%d,yy=%d,mp=%d\n", xx, yy, mp[xx][yy]);
                 status += (1 << mp[xx][yy] - 1);
@@ -77,59 +77,4 @@ int shortestPathAllKeys(vector<string> &grid)
         }
     }
     return -1;
-}
-int n = grid.size();
-int m = grid[0].size();
-queue<int> qu;
-for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < m; j++)
-    {
-        if (grid[i][j] == '.')
-            mp[i][j] = 0;
-        if (grid[i][j] == '#')
-            mp[i][j] = -1;
-        if (grid[i][j] == '@')
-            mp[i][j] = 0, qu.push(i), qu.push(j), qu.push(0), qu.push(0);
-        if ('A' <= grid[i][j] && grid[i][j] <= 'F')
-            mp[i][j] = grid[i][j] - 'A' + 10 + 1;
-        if ('a' <= grid[i][j] && grid[i][j] <= 'f')
-            mp[i][j] = grid[i][j] - 'a' + 1;
-    }
-}
-while (!qu.empty())
-{
-    int x, y, status, val;
-    x = qu.front();
-    qu.pop();
-    y = qu.front();
-    qu.pop();
-    status = qu.front();
-    qu.pop();
-    val = qu.front();
-    qu.pop();
-    printf("x=%d,y=%d,status=%o,val=%d\n", x, y, val, status);
-    for (int i = 0; i < 4; i++)
-    {
-        int xx = x + X[i];
-        int yy = y + Y[i];
-        if (xx < 0 || x >= n || yy < 0 || yy >= m)
-            continue;
-        if (mp[xx][yy] != -1 || dp[xx][yy][status] == 1)
-            continue;
-        if (mp[xx][yy] > 10 && (status & (1 << (mp[xx][yy] - 10) == 0)))
-            continue;
-        dp[xx][yy][status] == 1;
-        if (mp[xx][yy] > 0 && mp[xx][yy] < 7)
-        {
-            status &= (1 << mp[xx][yy]);
-            if ((status & 0b111111) == 0b111111)
-                return val + 1;
-        }
-        qu.push(xx);
-        qu.push(yy);
-        qu.push(status);
-        qu.push(val + 1);
-        dp[xx][yy][status] == 1;
-    }
 }
