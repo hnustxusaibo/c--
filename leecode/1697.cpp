ba8d481519de31e9aa1941e4231d513dd25afe41
saibo-xu <saibo.xu@yitu-inc.com>
2022-12-15 16:42:08 +0800

    sss

---
+++ leecode/1697.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+const int mod = 1e9 + 7;
+const int MAX = 2e9 + 7;
+const int N = 1e5 + 5;
+
+int diff[N], dp[N];
+deque<pair<int, int>> dequ;
+
+vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>> &edgeList, vector<vector<int>> &queries)
+{
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<vector<int>> num = {{2, 4}, {2, 5}, {3, 1}, {3, 2}, {3, 7}, {3, 1}, {4, 4}, {1, 3}, {5, 2}};
+    // boxDelivering(num, 5, 5, 7);
+    return 0;
+}
