e532a3c471ffe7c13c94fa14f7cd5c30c22192ac
saibo-xu <saibo.xu@yitu-inc.com>
2023-03-23 10:35:04 +0800

    commit

--- main.cpp
+++ main.cpp
@@ -1,39 +1,56 @@
-#include <bits/stdc++.h>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <thread>
 
 using namespace std;
 
-typedef long long ll;
-typedef pair<ll, ll> pll;
-typedef pair<int, int> pii;
-
-const int mod = 1e9 + 7;
-long long dp[1002][1002];
-bool f[1002][1002];
-int dfs(int n, int k)
-{
-    long ans = 0;
-    for (int i = 0; i <= n && i <= k; i++)
-    {
-        if (f[n - 1][k - 1])
-            ans = (ans + dp[n - 1][k - i]) % mod;
-        else
-            ans = (ans + dfs(n - 1, k - i)) % mod;
-    }
-    dp[n][k] = ans;
-    f[n][k] = 1;
-    return ans;
-}
-
-int kInversePairs(int n, int k)
-{
-    for (int i = 0; i <= n; i++)
-        f[0][i] = 1;
-    return dfs(n, k);
-}
-
 int main()
 {
-    printf("Hello World!\n");
-    cout << "Jell" << endl;
+    const long long N = 1e8; // 数组大小
+    int *arr = new int[N];   // 动态分配数组内存
+    int *tem = new int[N];
+
+    // 用时间作为随机数种子，以确保每次运行结果不同
+    std::srand(std::time(nullptr));
+
+    // 生成随机数
+    for (long long i = 0; i < N; ++i)
+    {
+        arr[i] = std::rand();     // 生成随机数
+        tem[i] = std::rand() % N; // 随机访问位置
+
+        if (i % (N / 100) == 0)
+        {
+            std::cout << "\rrunning " << i / (N / 100) << '%'; // 使用 \r 实现覆盖输出
+            std::flush(std::cout);
+        }
+    }
+
+    std::cout << std::endl; // 输出换行符
+
+    // 暂停程序5秒
+    cout << "wait 5s" << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+    cout << "start add" << endl;
+    // 记录起始时间
+    clock_t start_time = clock();
+
+    // 累加求和
+    long long sum = 0;
+    for (long long i = 0; i < N; ++i)
+    {
+        sum = (1ll * sum + arr[tem[i]]) % N;
+    }
+
+    // 计算所用时间并输出求和结果和所用时间
+    double elapsed_time = static_cast<double>(clock() - start_time) / CLOCKS_PER_SEC;
+    std::cout << "Sum: " << sum << '\n';
+    std::cout << "Time: " << elapsed_time << " seconds\n";
+
+    delete[] arr; // 释放动态分配的数组内存
+
     return 0;
 }
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def restoreMatrix(self, rowSum: list[int], colSum: list[int]) -> list[list[int]]:
+    n = len(rowSum)
+    m = len(colSum)
+    ans = []
+    for i in range(0, n):
+        ans.append([])
+        for j in range(0, m):
+            mi = min(rowSum[i], colSum[j])
+            ans[i].append(mi)
+            rowSum[i] -= mi
+            rowSum[j] -= mi
+
+
 if __name__ == '__main__':
     while (True):
         a = int(input())
